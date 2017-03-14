91d7a64fbf646944a9007d11f13d5f6fdd4b4a7e
Thanos983 <Thanos983@hotmail.gr>
2017-03-15 01:27:06 +0200

    added evaluation function

--- schedule.py
+++ schedule.py
@@ -1,1 +1,20 @@
-#My first serious program in .py
+# Scheduling problem rewritten in Python
+
+file = 'test.txt'
+activities = {}
+
+with open(file, 'r') as f:
+    for line in f:
+        line = line.split()
+        activities[line[0]] = line[1::]
+
+
+def can_activity_be_done(activities, path):
+
+    for a in activities.keys():
+        if a not in path:
+            for prereq in a[1::]:
+                if prereq not in path:
+                    return False
+
+            return True  # it can be done
